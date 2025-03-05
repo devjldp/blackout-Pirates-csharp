5b3c996027c8ba5100ad25db34e061586b882779
JoseDev <132334488+devjldp@users.noreply.github.com>
2025-03-05 10:33:05 +0000

    Modify header home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
  <div>
-    <h1 className="text-white">Home</h1>
+    <h1 className="main-header">BlackOut Pirates</h1>
+    <h3 className='text-white'>☠️ Embrace the pirate life! ☠️</h3>
     {/* <img src="./public/assets/images/mainImage.webp" alt="Band main image" id='main-image'/> */}
  </div>
   )
